2525c967d70c55ffd5f68af58e7985d6ad84c3b7
Jonathan Sebastian Pastas <jpastas@est.ups.edu.ec>
2021-07-07 21:29:21 -0500

    Añadido Archivo de Base de Datos y creando sesion

---
+++ bdatos.py
@@ -0,0 +1,56 @@
+import pymysql
+
+db = pymysql.connect(
+    host='remotemysql.com',
+    user='3LuICOPYq0',
+    password="Fh6aYfHmFi",
+    db='3LuICOPYq0',
+)
+
+
+def queryDatos(user, contra):
+    sql = "SELECT * FROM clientes WHERE correo='"+str(user)+"'"
+    cursor = db.cursor()
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    for row in data:
+
+        email=row[6]
+        contrasenia=row[7]
+
+        print(email)
+        print(contrasenia)
+        if email == user and contrasenia == contra:
+            return True
+        else:
+            return False
+
+
+def perfiluser(user):
+
+    dato=0
+    sql = "SELECT * FROM clientes WHERE correo='"+str(user)+"'"
+    cursor = db.cursor()
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    for row in data:
+        dato = row[1]
+    return dato
+
+def nombreuser(user):
+    sql = "SELECT * FROM clientes WHERE correo='" + str(user) + "'"
+    cursor = db.cursor()
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    for row in data:
+        nombre = row[2]+" "+row[3]
+    return nombre
+
+def ciuser(user):
+    sql = "SELECT * FROM clientes WHERE correo='" + str(user) + "'"
+    cursor = db.cursor()
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    for row in data:
+        ci = row[0]
+    return ci
--- main.py
+++ main.py
@@ -1,19 +1,60 @@
-from flask import Flask, render_template
+from flask import Flask, render_template ,request, session, redirect, url_for,flash
 import hashlib
 
-app = Flask(__name__)
+from bdatos import *
 
+app = Flask(__name__)
+app.secret_key='jsp1234'
 
 @app.route('/')
 def index() -> 'html':
     result = hashlib.md5(b'1234')
     print('equivale', result.digest())
-    return render_template('index.html', titulo='Tienda de Ropa Cracks')
+    return render_template('index.html', titulo='HomeSmart')
 
 
 @app.route('/login')
 def login() -> 'html':
     return render_template('login.html', titulo='Iniciar Sesión')
+
+@app.route('/ingresar' ,methods=['POST'])
+def ingreso() -> 'html':
+  user=request.form['correo']
+  clave = request.form['clave']
+  validar=queryDatos(user,clave)
+  #print(validar)
+
+  #print(nombre)
+
+  #print(perfil)
+
+  if validar == True:
+      perfilu = perfiluser(user)
+      nombre = nombreuser(user)
+      cedula = ciuser(user)
+      session['username']=user
+      session['pass']=clave
+      session['perfiluser']=perfilu
+      session['nombre']=nombre
+      session['ci']=cedula
+      return redirect(url_for('paginicio'))
+  else :
+       flash("Error al Inciar Sesion. Por Favor Verifique sus Credenciales de Acceso")
+       return  redirect(url_for('login'))
+
+@app.route('/inicio')
+def paginicio() -> 'html':
+
+    if 'username' in session :
+        print("true")
+        #menu=menuopciones(str(session['perfiluser']))
+        usuario=session['username']
+        nom = str(session['nombre'])
+        return render_template('cliente.html')
+
+
+    else :
+        return redirect(url_for('index'))
 
 
 app.run(debug=True)
